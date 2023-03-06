6d03b4969a22a539ac1aa3b18661d464d7751510
Vladislav Ma-iu-shan <vlad294@gmail.com>
2023-03-06 22:36:13 +0200

    API Limit on input file size

--- api/src/main/kotlin/models/validation/CreateExecutionJobValidation.kt
+++ api/src/main/kotlin/models/validation/CreateExecutionJobValidation.kt
@@ -8,29 +8,38 @@ import kiso.core.models.Language
 fun Application.configureExecutionJobValidation() {
     install(RequestValidation) {
         validate<CreateExecutionJob> { request ->
-            if (Language.valueOfOrNull(request.language) == null) {
-                return@validate ValidationResult.Invalid(
-                    "Invalid language, supported languages: ${Language.values().joinToString()}"
-                )
-            }
-
-            fun isValidNameOrVersion(value: String): Boolean =
-                value.all { it.isLetterOrDigit() || arrayOf('.', '-', '_').contains(it) }
-
-            if(request.packages == null) {
-                return@validate ValidationResult.Valid
-            }
-            for (pkg in request.packages) {
-                if (!isValidNameOrVersion(pkg.name)) {
-                    return@validate ValidationResult.Invalid("Invalid package name")
-                }
-
-                if (pkg.version != null && !isValidNameOrVersion(pkg.version!!)) {
-                    return@validate ValidationResult.Invalid("Invalid package version")
-                }
-            }
-
-            return@validate ValidationResult.Valid
+            return@validate validateRequest(request)
         }
     }
+}
+
+private const val MAX_FILE_SIZE = 10 * 1024 * 1024 // 10 megabytes
+private fun validateRequest(request: CreateExecutionJob): ValidationResult {
+    if (Language.valueOfOrNull(request.language) == null) {
+        return ValidationResult.Invalid(
+            "Invalid language, supported languages: ${Language.values().joinToString()}"
+        )
+    }
+
+    if (request.sourceCode.length > MAX_FILE_SIZE) {
+        return ValidationResult.Invalid("Input file is too big")
+    }
+
+    fun isValidNameOrVersion(value: String): Boolean =
+        value.all { it.isLetterOrDigit() || arrayOf('.', '-', '_').contains(it) }
+
+    if (request.packages == null) {
+        return ValidationResult.Valid
+    }
+    for (pkg in request.packages) {
+        if (!isValidNameOrVersion(pkg.name)) {
+            return ValidationResult.Invalid("Invalid package name")
+        }
+
+        if (pkg.version != null && !isValidNameOrVersion(pkg.version!!)) {
+            return ValidationResult.Invalid("Invalid package version")
+        }
+    }
+
+    return ValidationResult.Valid
 }
--- reducer/src/main/kotlin/config/ReducerConfig.kt
+++ reducer/src/main/kotlin/config/ReducerConfig.kt
@@ -3,5 +3,5 @@ package kiso.reducer.config
 import java.net.InetAddress
 
 object ReducerConfig {
-    val instanceName = InetAddress.getLocalHost().hostName
+    val instanceName: String = InetAddress.getLocalHost().hostName
 }
